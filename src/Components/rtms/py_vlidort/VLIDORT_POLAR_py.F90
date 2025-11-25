c066bc7c42be958a86bab1df02e3376ca3598df1
Patricia Castellanos <patricia.castellanos@nasa.gov>
2025-11-25 15:21:12 -0500

    getting rid the no cloud MODIS BRDF driver.  must define cloud inputs now.

--- src/Components/rtms/py_vlidort/VLIDORT_POLAR_py.F90
+++ src/Components/rtms/py_vlidort/VLIDORT_POLAR_py.F90
@@ -40,13 +40,13 @@ subroutine ROT_CALC(km, nch, nobs, channels, pe, he, te, &
 
 end subroutine ROT_CALC
 
-subroutine VECTOR_BRDF_MODIS_CLOUD(km, nch, nobs, channels, nstreams, plane_parallel, nMom, nPol,nkernel,nparam, &
+subroutine VECTOR_BRDF_MODIS(km, nch, nobs, channels, nstreams, plane_parallel, nMom, nPol,nkernel,nparam, &
                      ROT, depol, alpha, tau, ssa, pmom, tauI, ssaI, pmomI, tauL, ssaL, pmomL, &
                      pe, he, te, kernel_wt, param, &
                      solar_zenith, relat_azymuth, sensor_zenith, flux_factor, &
                      MISSING,verbose, radiance_VL_SURF,reflectance_VL_SURF, BR, Q, U, BR_Q, BR_U, rc)
 
-    use VLIDORT_BRDF_MODIS, only: VLIDORT_Vector_LandMODIS_Cloud
+    use VLIDORT_BRDF_MODIS, only: VLIDORT_Vector_LandMODIS
     implicit None
 
   ! !INPUT PARAMETERS:
@@ -118,7 +118,7 @@ subroutine VECTOR_BRDF_MODIS_CLOUD(km, nch, nobs, channels, nstreams, plane_para
     real*8,           intent(out)           :: BR_U(nobs, nch)                ! Stokes parameter U
 
 
-    call VLIDORT_Vector_LandMODIS_Cloud (km, nch, nobs, channels, nstreams, plane_parallel, nMom, &
+    call VLIDORT_Vector_LandMODIS (km, nch, nobs, channels, nstreams, plane_parallel, nMom, &
                                    nPol, ROT, depol, alpha, tau, ssa, pmom, tauI, ssaI, pmomI, tauL, ssaL, pmomL, &
                                    pe, he, te, &
                                    kernel_wt, param, &
@@ -130,82 +130,6 @@ subroutine VECTOR_BRDF_MODIS_CLOUD(km, nch, nobs, channels, nstreams, plane_para
                                    radiance_VL_SURF, &
                                    reflectance_VL_SURF, &
                                    BR, Q, U, BR_Q, BR_U, rc )
-
-
-end subroutine VECTOR_BRDF_MODIS_CLOUD
-
-subroutine VECTOR_BRDF_MODIS(km, nch, nobs, channels, nstreams, plane_parallel, nMom, nPol,nkernel,nparam, &
-                     ROT, depol, tau, ssa, pmom, pe, he, te, kernel_wt, param, &
-                     solar_zenith, relat_azymuth, sensor_zenith, &
-                     MISSING,verbose, radiance_VL_SURF,reflectance_VL_SURF, BR, Q, U, BR_Q, BR_U, rc)
-
-    use VLIDORT_BRDF_MODIS, only: VLIDORT_Vector_LandMODIS  
-    implicit None
-
-  ! !INPUT PARAMETERS:
-
-    integer,          intent(in)            :: km    ! number of vertical levels 
-    integer,          intent(in)            :: nch   ! number of channels
-    integer,          intent(in)            :: nobs  ! number of observations
-
-    logical,          intent(in)            :: plane_parallel ! do plane parallel flag
-
-    integer,          intent(in)            :: nMom  ! number of phase function moments 
-    integer,          intent(in)            :: nPol  ! number of scattering matrix components                               
-    integer,          intent(in)            :: nstreams  ! number of half space streams
-
-    integer,          intent(in)            :: nkernel ! number of kernels
-    integer,          intent(in)            :: nparam  ! number of kernel parameters
-                    
-    real*8,           intent(in)            :: channels(nch)    ! wavelengths [nm]
-
-  !                                                   ! --- Rayleigh Parameters ---
-    real*8,           intent(in)            :: ROT(km,nobs,nch) ! rayleigh optical thickness
-    real*8,           intent(in)            :: depol(nch)       ! rayleigh depolarization ratio
-
-  !                                                   ! --- Aerosol Optical Properties ---
-    real*8,           intent(in)            :: tau(km,nch,nobs) ! aerosol optical depth
-    real*8,           intent(in)            :: ssa(km,nch,nobs) ! single scattering albedo    
-    real*8,           intent(in)            :: pmom(km,nch,nobs,nMom,nPol) !components of the scat phase matrix
-
-    real*8,           intent(in)            :: MISSING          ! MISSING VALUE
-    real*8,           intent(in)            :: pe(km+1,nobs)    ! pressure at layer edges [Pa]
-    real*8,           intent(in)            :: he(km+1,nobs)    ! height above sea-level  [m]
-    real*8,           intent(in)            :: te(km+1,nobs)    ! temperature at layer edges [K]
-
-    real*8,           intent(in)            :: kernel_wt(nkernel,nch,nobs)    ! kernel weights (/fiso,fgeo,fvol/)
-    real*8,           intent(in)            :: param(nparam,nch,nobs)         ! Li-Sparse parameters 
-                                                                              ! param1 = crown relative height (h/b)
-                                                                              ! param2 = shape parameter (b/r)
-                         
-    real*8,           intent(in)            :: solar_zenith(nobs)  
-    real*8,           intent(in)            :: relat_azymuth(nobs) 
-    real*8,           intent(in)            :: sensor_zenith(nobs) 
-
-    integer,          intent(in)            :: verbose
-
-  ! !OUTPUT PARAMETERS:
-    real*8,           intent(out)           :: radiance_VL_SURF(nobs,nch)     ! TOA normalized radiance from VLIDORT using surface module
-    real*8,           intent(out)           :: reflectance_VL_SURF(nobs, nch) ! TOA reflectance from VLIDORT using surface module
-    integer,          intent(out)           :: rc                             ! return code
-
-    real*8,           intent(out)           :: BR(nobs,nch)                   ! bidirectional reflectance 
-    real*8,           intent(out)           :: Q(nobs, nch)                   ! Stokes parameter Q
-    real*8,           intent(out)           :: U(nobs, nch)                   ! Stokes parameter U   
-    real*8,           intent(out)           :: BR_Q(nobs, nch)                ! Stokes parameter Q
-    real*8,           intent(out)           :: BR_U(nobs, nch)                ! Stokes parameter U   
-
-
-    call VLIDORT_Vector_LandMODIS (km, nch, nobs, channels, nstreams, plane_parallel, nMom, &
-                                   nPol, ROT, depol, tau, ssa, pmom, pe, he, te, &
-                                   kernel_wt, param, &
-                                   solar_zenith, &
-                                   relat_azymuth, &
-                                   sensor_zenith, &
-                                   MISSING,verbose, &
-                                   radiance_VL_SURF, &
-                                   reflectance_VL_SURF, &
-                                   BR, Q, U, BR_Q, BR_U, rc )  
 
 
 end subroutine VECTOR_BRDF_MODIS
