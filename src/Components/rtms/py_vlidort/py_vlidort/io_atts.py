1f39987a5fc3dc1bdf7427e8b44ed40ace9acbe0
Patricia Castellanos <patricia.castellanos@nasa.gov>
2026-07-21 17:26:24 -0400

    add output attributes dictionary

--- src/Components/rtms/py_vlidort/py_vlidort/io_atts.py
+++ src/Components/rtms/py_vlidort/py_vlidort/io_atts.py
@@ -99,3 +99,76 @@ ARGS_ATTS : {
                         "missing_value": MISSING,
             },
     }
+
+OUT_ATTS:  {
+        "toa" : {
+                        "standard_name": "TOA Reflectance",
+                        "long_name": "reflectance at the top of the atmosphere",
+                        "units": "None",
+                        "missing_value": MISSING,
+                },
+
+        "I" : {
+                        "standard_name": "TOA I",
+                        "long_name": "intensity at the top of the atmosphere",
+                        "units": "W m-2 sr-1 nm-1",
+                        "missing_value": MISSING,
+                },
+        "Q" : {
+                        "standard_name": "TOA Q",
+                        "long_name": "Q-component of the stokes vector at the top of the atmopshere",
+                        "units": "W m-2 sr-1 nm-1",
+                        "missing_value": MISSING,
+                },
+
+        "U" : {
+                        "standard_name": "TOA U",
+                        "long_name": "U-component of the stokes vector at the top of the atmopshere",
+                        "units": "W m-2 sr-1 nm-1",
+                        "missing_value": MISSING,
+                },
+
+        "sref" : {
+                        "standard_name": "Surface Reflectance",
+                        "long_name": "Bi-Directional Surface Reflectance",
+                        "units": "None",
+                        "missing_value": MISSING,
+                },
+
+        "srefq" : {
+                        "standard_name": "Surface Reflectance Q",
+                        "long_name": "Bi-Directional Surface Reflectance Q",
+                        "units": "None",
+                        "missing_value": MISSING,
+                },
+
+        "srefu" : {
+                        "standard_name": "Surface Reflectance U",
+                        "long_name": "Bi-Directional Surface Reflectance U",
+                        "units": "None",
+                        "missing_value": MISSING,
+                },
+
+        "ch" : {
+                        "standard_name": "wavelength",
+                        "long_name": "wavelength",
+                        "units": "nm",
+                        "missing_value": MISSING,
+                },
+
+        "ts_I" : {
+                        "standard_name": "TWOSTREAM TOA I",
+                        "long_name": "intensity at the top of the atmosphere from TWOSTREAM",
+                        "units": "W m-2 sr-1 nm-1",
+                        "missing_value": MISSING,
+                },
+
+        "ts_toa" : {
+                        "standard_name": "TWOSTREAM TOA Reflectance",
+                        "long_name": "reflectance at the top of the atmosphere from TWOSTREAM",
+                        "units": "None",
+                        "missing_value": MISSING,
+                },
+
+    }
+
